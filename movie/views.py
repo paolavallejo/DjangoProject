478a1fa770ccaf7d8c1ee5b0bca0df29092ec51e
pvallej3 <pvallej3@eafit.edu.co>
2022-03-02 19:23:28 -0500

    uso de templates

--- movie/views.py
+++ movie/views.py
@@ -6,7 +6,9 @@ from django.http import HttpResponse
 def home(request): 
     #return HttpResponse('<h1>Welcome to Home Page</h1>')
     #return render(request, 'home.html')
-    return render(request, 'home.html', {'name':'Paola Vallejo'})
+    #return render(request, 'home.html', {'name':'Paola Vallejo'})
+    searchTerm = request.GET.get('searchMovie')
+    return render(request, 'home.html', {'searchTerm':searchTerm})
 
 def about(request): 
     return HttpResponse('<h1>Welcome to About Page</h1>')
